30b4576805977958c2f99d427c05838de20cee99
Rouzbeh-Berenji <berenji.ro@gmail.com>
2022-10-19 17:56:44 -0400

    countOnly

---
+++ countOnly.js
@@ -0,0 +1,45 @@
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    // inside the loop, 
+    // increment the counter for each item:
+    //   set a property with that string key to:
+    //     the value that was already there (or zero if nothing there) with 1 added to it.
+    // inside the loop:
+if (itemsToCount[item]) { 
+  if (results[item]) {
+    results[item] += 1;
+  } else {
+    results[item] = 1;
+  }
+}
+  }
+  return results;
+}
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅✅ Assertion Passed:[' + actual + '] === [' + expected + ']');
+  } else {
+    console.log('❌❌❌ Assertion Failed: [' + actual + '] !== [' + expected + ']');
+  }
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+// const results = {}
+// return results;
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+// console.log(result1["Jason"]);
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
